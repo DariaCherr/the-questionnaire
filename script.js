bc8d9997f22bf0f489365d8ed7f5f4f8ed30507b
DariaCherr <DaskhenCher@mail.ru>
2023-01-17 16:16:40 +0300

    Домашнее задание 10

--- script.js
+++ script.js
@@ -1,8 +1,42 @@
 const form = document.querySelector(".form");
-form.addEventListener("submit", (event) => {
-  // Предотвращает действие браузера по умолчанию. В данном случае — отправку формы
-  // https://learn.javascript.ru/default-browser-action
-  event.preventDefault();
+const sendName = document.querySelector("#name");
+const sendSecondName = document.querySelector("#secondName");
+const sendEmail = document.querySelector("#email");
+const sendPhone = document.querySelector("#phone");
 
-  // Здесь твой код
+form.addEventListener("submit", (event) => {
+    fetch(`http://46.21.248.81:3001/user`, {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer: DariaCherr'
+            },
+            body: JSON.stringify({
+                "name": sendName.value,
+                "secondName": sendSecondName.value,
+                "phone": sendPhone.value,
+                "email": sendEmail.value,
+                "agree": true
+            }),
+        })
+        .then((result) => {
+            return result.json();
+        })
+        .then((data) => {
+            console.log(data);
+            statusMessage.textContent = 'Загрузка данных прошла успешно!';
+            form.reset();
+        })
+        .catch((error) => {
+            console.log(error);
+            statusMessage.textContent = 'Ошибка! Данные не загрузились :(';
+        })
+    let statusMessage = document.createElement('div');
+    statusMessage.classList.add('message');
+    form.appendChild(statusMessage);
+    setTimeout(() => {
+        statusMessage.remove();
+    }, 5000);
+    event.preventDefault();
 });
